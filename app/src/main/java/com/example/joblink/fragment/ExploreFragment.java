ec7b2600947a9e06bbf10dba107327e288de4a32
Vat Vannaden <vat.vannaden.2822@rupp.edu.kh>
2025-10-21 19:08:40 +0700

    feat: Add new authentication and profile completion screens
    - Add Welcome, Forgot Password, Check Email, and Complete Profile activities and layouts.
    - Refactor Login and Create Account layouts to use `ConstraintLayout` for improved structure and responsiveness.
    - Introduce new drawables for UI elements, including radio buttons (`active`/`inactive`) and illustrative images for new screens (`forgot_password`, `check_email`).
    - Add a custom selector for radio button states (`custom_radio_selector.xml`).
    - Create a new text style `under_button_tx` and add new string resources.
    - Rename `ExploresFragment` to `ExploreFragment` and update its layout.
    - Adjust `drawer_menu.xml` item IDs for clarity.

--- app/src/main/java/com/example/joblink/fragment/ExploreFragment.java
+++ app/src/main/java/com/example/joblink/fragment/ExploreFragment.java
@@ -12,10 +12,10 @@ import com.example.joblink.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ExploresFragment#newInstance} factory method to
+ * Use the {@link ExploreFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ExploresFragment extends Fragment {
+public class ExploreFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class ExploresFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ExploresFragment() {
+    public ExploreFragment() {
         // Required empty public constructor
     }
 
@@ -39,8 +39,8 @@ public class ExploresFragment extends Fragment {
      * @return A new instance of fragment ExploreFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ExploresFragment newInstance(String param1, String param2) {
-        ExploresFragment fragment = new ExploresFragment();
+    public static ExploreFragment newInstance(String param1, String param2) {
+        ExploreFragment fragment = new ExploreFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
