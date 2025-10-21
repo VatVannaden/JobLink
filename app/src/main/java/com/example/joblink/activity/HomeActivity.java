37735c772df960330f9f7d42b24d28677fc90413
Vat Vannaden <vat.vannaden.2822@rupp.edu.kh>
2025-10-21 19:17:28 +0700

    refactor: Clean up HomeActivity
    - Remove boilerplate code for layout inflation and window insets.
    - Delete unused imports.
    - Remove FirebaseAuth instance variable.

--- app/src/main/java/com/example/joblink/activity/HomeActivity.java
+++ app/src/main/java/com/example/joblink/activity/HomeActivity.java
@@ -9,18 +9,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.joblink.R;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
