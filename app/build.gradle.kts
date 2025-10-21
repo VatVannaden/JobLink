2abe146f807b3382ceb62dd3cef39a7a0eb3cd26
vatvannaden <vat.vannaden.2822@rupp.edu.kh>
2025-10-21 19:45:59 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,11 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
     namespace = "com.example.joblink"
     compileSdk = 36
-
-    buildFeatures {
-        viewBinding = true
-    }
 
     defaultConfig {
         applicationId = "com.example.joblink"
@@ -35,14 +32,26 @@ android {
     }
 }
 
+android {
+    buildFeatures {
+        viewBinding = true
+        dataBinding = false
+        compose = false
+    }
+}
+
+
+
 dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
 }
